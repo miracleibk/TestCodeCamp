a9ab700b06aa50f78053db45eac178113ef0902d
miracleibk <miracleosuji2@gmail.com>
2021-12-02 14:02:10 +0100

    world project,hello world project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Training on java automated test");
+        System.out.println("Hello world, this is my project");
     }
 }
