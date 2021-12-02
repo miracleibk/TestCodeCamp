403a001f6d0c5abb4bcb16ff1387e3ac7c2c731d
miracleibk <miracleosuji2@gmail.com>
2021-12-02 13:37:24 +0100

    test code camp java project 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("This is miracles project");
+        System.out.println("hello team postman");
     }
 }
